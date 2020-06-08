05f586acae61ac9c7deae4f3fc9db83b168bceb5
青山良太 <aoyamaryota@aoyamaryouta-no-MacBook-Air.local>
2020-06-08 10:33:15 +0900

    小数点をエラーにする記述が漏れておりましたので、追加致しました。

--- js/main.js
+++ js/main.js
@@ -1,33 +1,35 @@
-
 'use strict'
+    window.addEventListener('DOMContentLoaded', ()=>{
+    document.querySelector('#add').addEventListener('click',()=>{
+    const FizzVal = document.querySelector('[name=fizz]').value;
+    const BuzzVal = document.querySelector('[name=buzz]').value;
+    const reg=/^([1-9]\d*|0)$/;
+    const targetNode=document.querySelector('#target');
+    var n;
 
-        window.addEventListener('DOMContentLoaded', ()=>{
-        document.querySelector('#add').addEventListener('click',()=>{
-        const FizzNum = parseInt(document.querySelector('[name=fizz]').value);
-        const BuzzNum = parseInt(document.querySelector('[name=buzz]').value);
-        var targetNode=document.querySelector('#target');
-        var n;
-        while(n=targetNode.firstChild){
-          n.parentNode.removeChild(n);
-        }
-        if (isNaN(FizzNum) ||isNaN(BuzzNum)){
-            n=Object.assign(document.createElement('li'),{textContent:"整数値を入れてください"});
-            targetNode.appendChild(n);
-        }else{
-        for (let index = 1; index <= 99; index++) {
-            n=document.createElement('li');
-        if(index % FizzNum === 0 && index % BuzzNum === 0) {
-            n.textContent=`FizzBuzz ${index}`;
-        }else if(index % FizzNum === 0) {
-            n.textContent=`Fizz ${index}`;
-        }else if(index % BuzzNum === 0){
-            n.textContent=`Buzz ${index}`;
-        }else{
-            continue;
-       }
-        targetNode.appendChild(n);
-
+    while(n=targetNode.firstChild){
+    n.parentNode.removeChild(n);
+    }
+    if ( !reg.test(FizzVal) || !reg.test(BuzzVal)){
+      n=Object.assign(document.createElement('li'),{textContent:"整数値を入れてください"});
+      targetNode.appendChild(n);
+    }else{
+    const FizzNum = parseInt(FizzVal);
+    const BuzzNum = parseInt(BuzzVal);
+    
+    for (let index = 1; index <= 99; index++) {
+      n=document.createElement('li');
+    if(index % FizzNum === 0 && index % BuzzNum === 0) {
+      n.textContent=`FizzBuzz ${index}`;
+    }else if(index % FizzNum === 0) {
+      n.textContent=`Fizz ${index}`;
+    }else if(index % BuzzNum === 0){
+      n.textContent=`Buzz ${index}`;
+    }else{
+      continue;
+    }
+    targetNode.appendChild(n);
     }
   }
-});
+ });
 });
